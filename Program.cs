eaecf1639f8241b01241b569cb014d1396e55ac4
MarkSander <markwillem.sander@gmail.com>
2025-06-20 01:01:18 +0200

    optimized forward checking

--- Program.cs
+++ Program.cs
@@ -1,223 +1,248 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Timers;
+using System.Linq;
 
-namespace KillerSudokuSolver
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var cages = new List<KillerSudokuSolver.Cage>
+{
+    new(9,  new() { (0, 0), (1, 0) }),
+    new(28, new() { (0, 1), (0, 2), (1, 2), (2, 2), (1, 3) }),
+    new(7,  new() { (1, 1), (2, 1) }),
+    new(11, new() { (0, 3), (0, 4), (0, 5), (0, 6) }),
+    new(15, new() { (0, 7), (1, 7) }),
+    new(11, new() { (0, 8), (1, 8) }),
+    new(21, new() { (1, 4), (2, 4), (2, 3) }),
+    new(12, new() { (1, 5), (2, 5) }),
+    new(18, new() { (1, 6), (2, 6), (2, 7), (2, 8), (3, 7) }),
+    new(23, new() { (2, 0), (3, 0), (4, 0), (5, 0) }),
+    new(12, new() { (3, 1), (4, 1), (3, 2) }),
+    new(18, new() { (3, 3), (4, 3), (5, 3), (4, 2) }),
+    new(19, new() { (3, 4), (4, 4), (5, 4) }),
+    new(16, new() { (3, 5), (4, 5), (5, 5) }),
+    new(14, new() { (3, 6), (4, 6), (4, 7) }),
+    new(18, new() { (3, 8), (4, 8), (5, 8), (6, 8) }),
+    new(9,  new() { (5, 1), (5, 2) }),
+    new(12, new() { (5, 6), (5, 7) }),
+    new(44, new() { (6, 0), (7, 0), (8, 0), (6, 1), (7, 1), (8, 1), (6, 2), (7, 2) }),
+    new(15, new() { (6, 3), (7, 3) }),
+    new(16, new() { (6, 4), (7, 4), (6, 5) }),
+    new(11, new() { (6, 6), (6, 7) }),
+    new(9,  new() { (7, 5), (7, 6), (8, 6) }),
+    new(24, new() { (7, 7), (7, 8), (8, 7), (8, 8) }),
+    new(13, new() { (8, 2), (8, 3), (8, 4), (8, 5) })
+};
+
+
+        var solver = new KillerSudokuSolver(cages);
+        var stopwatch = Stopwatch.StartNew();
+        bool solved = solver.Solve();
+        stopwatch.Stop();
+
+        if (solved)
         {
-            
-            int[,] board = new int[9, 9]
-            {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0}
-            };
-
-            // Killer sudoku hokken
-            List<Cage> cages = new List<Cage>
-            {
-                // killer 1
-                //new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0), (1, 1) }),
-                //new Cage(10, new List<(int, int)> { (0, 2), (0, 3), (1, 2), (1, 3) }),
-
-                // killer 2
-                new Cage(9,  new List<(int, int)> { (0, 0), (1, 0) }),
-                new Cage(28, new List<(int, int)> { (0, 1), (0, 2), (1, 2), (2, 2), (1,3) }),
-                new Cage(7,  new List<(int, int)> { (1, 1), (2, 1) }),
-                new Cage(11, new List<(int, int)> { (0, 3), (0, 4), (0, 5), (0,6) }),
-                new Cage(15, new List<(int, int)> { (0, 7), (1, 7) }),
-                new Cage(11, new List<(int, int)> { (0, 8), (1, 8)}),
-                new Cage(21, new List<(int, int)> {(1, 4), (2,4), (2,3)}),
-                new Cage(12, new List<(int, int)> {(1, 5), (2,5) }),
-                new Cage(18, new List<(int, int)> {(1, 6), (2,6), (2,7), (2,8), (3,7) }),
-                new Cage(23, new List<(int, int)> {(2,0), (3,0), (4,0), (5,0) }),
-                new Cage(12, new List<(int, int)> {(3,1), (4, 1), (3, 2)}),
-                new Cage(18, new List<(int, int)> {(3, 3), (4, 3), (5, 3), (4,2)}),
-                new Cage(19, new List<(int, int)> {(3, 4), (4, 4), (5, 4)}),
-                new Cage(16, new List<(int, int)> {(3, 5), (4, 5), (5, 5)}),
-                new Cage(14, new List<(int, int)> {(3, 6), (4, 6), (4, 7)}),
-                new Cage(18, new List<(int, int)> {(3, 8), (4, 8), (5, 8), (6,8)}),
-                new Cage(9, new List<(int, int)> {(5, 1), (5, 2)}),
-                new Cage(12, new List<(int, int)> {(5, 6), (5, 7)}),
-                new Cage(44, new List<(int, int)> {(6, 0), (7, 0), (8, 0), (6,1), (7,1), (8,1), (6,2), (7,2)}),
-                new Cage(15, new List<(int, int)> {(6, 3), (7, 3)}),
-                new Cage(16, new List<(int, int)> {(6, 4), (7, 4), (6, 5)}),
-                new Cage(11, new List<(int, int)> {(6, 6), (6, 7)}),
-                new Cage(9, new List<(int, int)> {(7,5), (7, 6), (8, 6)}),
-                new Cage(24, new List<(int, int)> {(7, 7), (7, 8), (8,7), (8,8)}),
-                new Cage(13, new List<(int, int)> {(8, 2), (8, 3), (8, 4), (8,5)}),
-
-
-                // etc..
-            };
-
-            Stopwatch atimer = Stopwatch.StartNew();
-            KillerSudokuSolver solver = new KillerSudokuSolver(board, cages);
-            if (solver.Solve())
-            {
-                Console.WriteLine("Solved Killer Sudoku:");
-                solver.PrintBoard();
-            }
-            else
-            {
-                Console.WriteLine("No solution exists.");
-            }
-            atimer.Stop();
-            Console.WriteLine("Elapsed time: " + atimer.ElapsedMilliseconds + " ms");
+            solver.Print();
+            Console.WriteLine($"Solved in {stopwatch.Elapsed.TotalMilliseconds:F3} ms");
+            Console.ReadLine();
         }
+        else
+        {
+            Console.WriteLine("No solution found.");
+            Console.ReadLine();
+        }
+
     }
 
-    public class Cage
-    {
-        public int TargetSum { get; }
-        public List<(int, int)> Cells { get; }
+}
 
-        public Cage(int targetSum, List<(int, int)> cells)
-        {
-            TargetSum = targetSum;
-            Cells = cells;
-        }
+
+
+public class KillerSudokuSolver
+{
+    const int Size = 9;
+    int[,] Grid = new int[Size, Size];
+    Dictionary<(int, int), List<int>> Domains = new();
+    List<Cage> Cages;
+
+    public record Cage(int Sum, List<(int Row, int Col)> Cells);
+
+    public KillerSudokuSolver(List<Cage> cages)
+    {
+        Cages = cages;
+        InitializeDomains();
     }
 
-    public class KillerSudokuSolver
+    void InitializeDomains()
     {
-        private int[,] board;
-        private List<Cage> cages;
-        private const int SIZE = 9;
+        for (int r = 0; r < Size; r++)
+            for (int c = 0; c < Size; c++)
+                Domains[(r, c)] = Enumerable.Range(1, 9).ToList();
+    }
 
-        public KillerSudokuSolver(int[,] board, List<Cage> cages)
-        {
-            this.board = board;
-            this.cages = cages;
-        }
-        
-        public bool Solve()
-        {
-            for (int row = 0; row < SIZE; row++)
-            {
-                for (int col = 0; col < SIZE; col++)
-                {
-                    if (board[row, col] == 0)
-                    {
-                        for (int num = 1; num <= SIZE; num++)
-                        {
-                            if (IsSafe(row, col, num))
-                            {
-                                board[row, col] = num;
+    public bool Solve()
+    {
+        return Backtrack();
+    }
 
-                                if (Solve())
-                                {
-                                    return true;
-                                }
+    bool Backtrack()
+    {
+        var unassigned = Domains
+            .Where(kv => Grid[kv.Key.Item1, kv.Key.Item2] == 0)
+            .OrderBy(kv => kv.Value.Count)
+            .ToList();
 
-                                board[row, col] = 0; // backtrack
-                            }
-                        }
-                        return false;
-                    }
-                }
-            }
+        if (!unassigned.Any())
             return true; // Solved
+
+        var cell = unassigned.First().Key;
+        var domain = unassigned.First().Value;
+
+        foreach (var value in domain.ToList())
+        {
+            if (IsValid(cell.Item1, cell.Item2, value))
+            {
+                var backupGrid = (int[,])Grid.Clone();
+                var backupDomains = DeepCloneDomains();
+
+                Grid[cell.Item1, cell.Item2] = value;
+
+                if (ForwardCheck(cell.Item1, cell.Item2, value))
+                {
+                    if (Backtrack())
+                        return true;
+                }
+
+                Grid = backupGrid;
+                Domains = backupDomains;
+            }
+        }
+        return false;
+    }
+
+    bool IsValid(int row, int col, int value)
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            if (Grid[row, i] == value || Grid[i, col] == value)
+                return false;
         }
 
-        private bool IsSafe(int row, int col, int num)
+        int startRow = (row / 3) * 3, startCol = (col / 3) * 3;
+        for (int r = startRow; r < startRow + 3; r++)
+            for (int c = startCol; c < startCol + 3; c++)
+                if (Grid[r, c] == value)
+                    return false;
+
+        return true;
+    }
+
+    bool ForwardCheck(int row, int col, int value)
+    {
+        foreach (var pos in GetPeers(row, col))
         {
-            // Check row
-            for (int x = 0; x < SIZE; x++)
-            {
-                if (board[row, x] == num)
-                {
-                    return false;
-                }
-            }
-
-            // Check column
-            for (int x = 0; x < SIZE; x++)
-            {
-                if (board[x, col] == num)
-                {
-                    return false;
-                }
-            }
-
-            // Check 3x3 subgrid
-            int startRow = row - row % 3;
-            int startCol = col - col % 3;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (board[i + startRow, j + startCol] == num)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            // Check cages
-            foreach (var cage in cages)
-            {
-                if (cage.Cells.Contains((row, col)))
-                {
-                    int currentSum = 0;
-                    int emptyCells = 0;
-
-                    foreach (var cell in cage.Cells)
-                    {
-                        int r = cell.Item1;
-                        int c = cell.Item2;
-                        if (board[r, c] == num && (r != row || c != col))
-                        {
-                            return false;
-                        }
-                        if (board[r, c] == 0)
-                        {
-                            emptyCells++;
-                        }
-                        currentSum += board[r, c];
-                    }
-
-                    if (currentSum + num > cage.TargetSum)
-                    {
-                        return false;
-                    }
-
-                    if (emptyCells == 1 && currentSum + num != cage.TargetSum)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
+            Domains[pos].Remove(value);
+            if (!Domains[pos].Any() && Grid[pos.Item1, pos.Item2] == 0)
+                return false;
         }
 
-        public void PrintBoard()
+        foreach (var cage in Cages.Where(c => c.Cells.Contains((row, col))))
         {
-            for (int r = 0; r < SIZE; r++)
-            {
-                for (int d = 0; d < SIZE; d++)
-                {
-                    Console.Write(board[r, d]);
-                    Console.Write(" ");
-                }
-                Console.WriteLine();
+            var assigned = cage.Cells
+                .Where(p => Grid[p.Row, p.Col] != 0)
+                .Select(p => Grid[p.Row, p.Col])
+                .ToList();
 
-                if ((r + 1) % 3 == 0)
+            var unassigned = cage.Cells
+                .Where(p => Grid[p.Row, p.Col] == 0)
+                .ToList();
+
+            int remaining = cage.Sum - assigned.Sum();
+
+            if (unassigned.Count == 0)
+                return assigned.Sum() == cage.Sum && assigned.Distinct().Count() == assigned.Count;
+
+            var combos = GetValidCombinations(unassigned.Count, remaining, assigned);
+            var allowed = combos.SelectMany(c => c).ToHashSet();
+
+            foreach (var cell in unassigned)
+            {
+                Domains[cell] = Domains[cell].Where(d => allowed.Contains(d)).ToList();
+                if (!Domains[cell].Any())
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    List<List<int>> GetValidCombinations(int count, int sum, List<int> exclude)
+    {
+        return Enumerable.Range(1, 9)
+            .Where(i => !exclude.Contains(i))
+            .ToList()
+            .Combinations(count)
+            .Where(c => c.Sum() == sum)
+            .ToList();
+    }
+
+    List<(int, int)> GetPeers(int row, int col)
+    {
+        var peers = new HashSet<(int, int)>();
+
+        for (int i = 0; i < Size; i++)
+        {
+            if (i != col) peers.Add((row, i));
+            if (i != row) peers.Add((i, col));
+        }
+
+        int startRow = (row / 3) * 3, startCol = (col / 3) * 3;
+        for (int r = startRow; r < startRow + 3; r++)
+            for (int c = startCol; c < startCol + 3; c++)
+                if (r != row || c != col) peers.Add((r, c));
+
+        return peers.ToList();
+    }
+
+    Dictionary<(int, int), List<int>> DeepCloneDomains()
+    {
+        return Domains.ToDictionary(
+            kv => kv.Key,
+            kv => new List<int>(kv.Value)
+        );
+    }
+
+    public void Print()
+    {
+        for (int r = 0; r < Size; r++)
+        {
+            for (int c = 0; c < Size; c++)
+                Console.Write($"{Grid[r, c]} ");
+            Console.WriteLine();
+        }
+    }
+}
+
+public static class Extensions
+{
+    public static IEnumerable<List<T>> Combinations<T>(this List<T> list, int length)
+    {
+        if (length == 0) yield return new List<T>();
+        else
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var head = list[i];
+                var tail = list.Skip(i + 1).ToList();
+                foreach (var tailComb in tail.Combinations(length - 1))
                 {
-                    Console.WriteLine();
+                    tailComb.Insert(0, head);
+                    yield return tailComb;
                 }
             }
         }
     }
 }
+
