24b59e19de88bba8ef7e45a8419586f5d3e2687c
MarkSander <markwillem.sander@gmail.com>
2025-06-19 16:24:23 +0200

    updated forward checking

--- ForwardTracking.cs
+++ ForwardTracking.cs
@@ -1,200 +1,112 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Sudoku
+public class Cage
 {
-    public class ForwardTracking
+    public int TargetSum { get; }
+    public List<(int, int)> Cells { get; }
+
+    public Cage(int targetSum, List<(int, int)> cells)
     {
-        public void RunForwardTracking()
-        {
+        TargetSum = targetSum;
+        Cells = cells;
+    }
+}
 
-            int[,] board = new int[9, 9]
-            {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0}
-            };
+public class ForwardTracking
+{
+    private const int Size = 9;
+    private int[,] board = new int[Size, Size];
+    private List<Cage> cages;
 
-            // Killer sudoku hokken
-            List<Cage> cages = new List<Cage>
-            {
-                // killer 1
-                //new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0), (1, 1) }),
-                //new Cage(10, new List<(int, int)> { (0, 2), (0, 3), (1, 2), (1, 3) }),
-
-                // killer 2
-                new Cage(9,  new List<(int, int)> { (0, 0), (1, 0) }),
-                new Cage(28, new List<(int, int)> { (0, 1), (0, 2), (1, 2), (2, 2), (1,3) }),
-                new Cage(7,  new List<(int, int)> { (1, 1), (2, 1) }),
-                new Cage(11, new List<(int, int)> { (0, 3), (0, 4), (0, 5), (0,6) }),
-                new Cage(15, new List<(int, int)> { (0, 7), (1, 7) }),
-                new Cage(11, new List<(int, int)> { (0, 8), (1, 8)}),
-                new Cage(21, new List<(int, int)> {(1, 4), (2,4), (2,3)}),
-                new Cage(12, new List<(int, int)> {(1, 5), (2,5) }),
-                new Cage(18, new List<(int, int)> {(1, 6), (2,6), (2,7), (2,8), (3,7) }),
-                new Cage(23, new List<(int, int)> {(2,0), (3,0), (4,0), (5,0) }),
-                new Cage(12, new List<(int, int)> {(3,1), (4, 1), (3, 2)}),
-                new Cage(18, new List<(int, int)> {(3, 3), (4, 3), (5, 3), (4,2)}),
-                new Cage(19, new List<(int, int)> {(3, 4), (4, 4), (5, 4)}),
-                new Cage(16, new List<(int, int)> {(3, 5), (4, 5), (5, 5)}),
-                new Cage(14, new List<(int, int)> {(3, 6), (4, 6), (4, 7)}),
-                new Cage(18, new List<(int, int)> {(3, 8), (4, 8), (5, 8), (6,8)}),
-                new Cage(9, new List<(int, int)> {(5, 1), (5, 2)}),
-                new Cage(12, new List<(int, int)> {(5, 6), (5, 7)}),
-                new Cage(44, new List<(int, int)> {(6, 0), (7, 0), (8, 0), (6,1), (7,1), (8,1), (6,2), (7,2)}),
-                new Cage(15, new List<(int, int)> {(6, 3), (7, 3)}),
-                new Cage(16, new List<(int, int)> {(6, 4), (7, 4), (6, 5)}),
-                new Cage(11, new List<(int, int)> {(6, 6), (6, 7)}),
-                new Cage(9, new List<(int, int)> {(7,5), (7, 6), (8, 6)}),
-                new Cage(24, new List<(int, int)> {(7, 7), (7, 8), (8,7), (8,8)}),
-                new Cage(13, new List<(int, int)> {(8, 2), (8, 3), (8, 4), (8,5)}),
-
-
-                // etc..
-            };
-            KillerSudokuSolver solver = new KillerSudokuSolver(board, cages);
-            if (solver.Solve())
-            {
-                Console.WriteLine("Solved Killer Sudoku:");
-                solver.PrintBoard();
-            }
-            else
-            {
-                Console.WriteLine("No solution exists.");
-            }
-        }
+    public ForwardTracking(List<Cage> cages)
+    {
+        this.cages = cages;
     }
 
-    public class Cage
+    public bool Solve()
     {
-        public int TargetSum { get; }
-        public List<(int, int)> Cells { get; }
-
-        public Cage(int targetSum, List<(int, int)> cells)
-        {
-            TargetSum = targetSum;
-            Cells = cells;
-        }
+        return ForwardCheck(0, 0);
     }
 
-    public class KillerSudokuSolver
+    private bool ForwardCheck(int row, int col)
     {
-        private int[,] board;
-        private List<Cage> cages;
-
-        public KillerSudokuSolver(int[,] board, List<Cage> cages)
-        {
-            this.board = board;
-            this.cages = cages;
-        }
-        public bool Solve()
-        {
-            return SolveInternal();
-        }
-
-        private bool SolveInternal()
-        {
-            (int row, int col)? cell = FindNextCell();
-            if (cell == null)
-                return true;
-
-            var (r, c) = cell.Value;
-            foreach (var val in Enumerable.Range(1, 9))
-            {
-                if (IsValid(r, c, val))
-                {
-                    board[r, c] = val;
-                    if (SolveInternal())
-                        return true;
-                    board[r, c] = 0;
-                }
-            }
-            return false;
-        }
-
-        private (int, int)? FindNextCell()
-        {
-            (int row, int col)? bestCell = null;
-            int minOptions = 10;
-
-            for (int r = 0; r < 9; r++)
-            {
-                for (int c = 0; c < 9; c++)
-                {
-                    if (board[r, c] == 0)
-                    {
-                        int options = Enumerable.Range(1, 9).Count(v => IsValid(r, c, v));
-                        if (options < minOptions)
-                        {
-                            minOptions = options;
-                            bestCell = (r, c);
-                            if (options == 1) return bestCell;
-                        }
-                    }
-                }
-            }
-            return bestCell;
-        }
-
-        private bool IsValid(int row, int col, int val)
-        {
-            for (int i = 0; i < 9; i++)
-                if (board[row, i] == val || board[i, col] == val)
-                    return false;
-
-            int boxRow = row / 3 * 3;
-            int boxCol = col / 3 * 3;
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    if (board[boxRow + i, boxCol + j] == val)
-                        return false;
-
-            foreach (var cage in cages)
-            {
-                if (cage.Cells.Contains((row, col)))
-                {
-                    int sum = val;
-                    HashSet<int> used = new HashSet<int> { val };
-                    foreach (var (r, c) in cage.Cells)
-                    {
-                        if ((r, c) == (row, col)) continue;
-                        int v = board[r, c];
-                        if (v != 0)
-                        {
-                            if (used.Contains(v)) return false;
-                            used.Add(v);
-                            sum += v;
-                        }
-                    }
-
-                    if (sum > cage.TargetSum) return false;
-                    if (used.Count == cage.Cells.Count && sum != cage.TargetSum)
-                        return false;
-                }
-            }
-
+        if (row == Size)
             return true;
-        }
-        public void PrintBoard()
+
+        int nextRow = col == Size - 1 ? row + 1 : row;
+        int nextCol = col == Size - 1 ? 0 : col + 1;
+
+        if (board[row, col] != 0)
+            return ForwardCheck(nextRow, nextCol);
+
+        for (int num = 1; num <= 9; num++)
         {
-            for (int r = 0; r < 9; r++)
+            board[row, col] = num;
+            if (IsValid(row, col) && ForwardCheck(nextRow, nextCol))
+                return true;
+            board[row, col] = 0;
+        }
+
+        return false;
+    }
+
+    private bool IsValid(int row, int col)
+    {
+        int val = board[row, col];
+
+        // Check row and column
+        for (int i = 0; i < Size; i++)
+        {
+            if (i != col && board[row, i] == val) return false;
+            if (i != row && board[i, col] == val) return false;
+        }
+
+        // Check 3x3 box
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int i = boxRow; i < boxRow + 3; i++)
+        {
+            for (int j = boxCol; j < boxCol + 3; j++)
             {
-                for (int c = 0; c < 9; c++)
-                {
-                    Console.Write(board[r, c] + " ");
-                }
-                Console.WriteLine();
+                if ((i != row || j != col) && board[i, j] == val) return false;
             }
         }
+
+        // Check cage constraint
+        foreach (var cage in cages)
+        {
+            if (cage.Cells.Any(cell => cell.Item1 == row && cell.Item2 == col))
+            {
+                int sum = 0;
+                var values = new HashSet<int>();
+                foreach (var (r, c) in cage.Cells)
+                {
+                    int v = board[r, c];
+                    if (v != 0)
+                    {
+                        if (!values.Add(v)) return false;
+                        sum += v;
+                    }
+                }
+                if (values.Count == cage.Cells.Count && sum != cage.TargetSum) return false;
+                if (sum > cage.TargetSum) return false;
+            }
+        }
+
+        return true;
     }
+
+    public void PrintBoard()
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
+}*/
--- Program.cs
+++ Program.cs
@@ -1,226 +1,246 @@
-﻿using Sudoku;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Timers;
+using System.Linq;
 
-namespace KillerSudokuSolver
+public class Cage
 {
-    class Program
+    public int TargetSum { get; }
+    public List<(int, int)> Cells { get; }
+
+    public Cage(int targetSum, List<(int, int)> cells)
     {
-        static void Main(string[] args)
+        TargetSum = targetSum;
+        Cells = cells;
+    }
+}
+
+public class KillerSudokuSolver
+{
+    private const int Size = 9;
+    private int[,] board = new int[Size, Size];
+    private List<Cage> cages;
+    private Dictionary<(int, int), Cage> cellToCage = new();
+
+    public KillerSudokuSolver(List<Cage> cages)
+    {
+        this.cages = cages;
+        foreach (var cage in cages)
         {
-            
-            int[,] board = new int[9, 9]
+            foreach (var cell in cage.Cells)
             {
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0}
-            };
-
-            // Killer sudoku hokken
-            List<Cage> cages = new List<Cage>
-            {
-                // killer 1
-                //new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0), (1, 1) }),
-                //new Cage(10, new List<(int, int)> { (0, 2), (0, 3), (1, 2), (1, 3) }),
-
-                // killer 2
-                new Cage(9,  new List<(int, int)> { (0, 0), (1, 0) }),
-                new Cage(28, new List<(int, int)> { (0, 1), (0, 2), (1, 2), (2, 2), (1,3) }),
-                new Cage(7,  new List<(int, int)> { (1, 1), (2, 1) }),
-                new Cage(11, new List<(int, int)> { (0, 3), (0, 4), (0, 5), (0,6) }),
-                new Cage(15, new List<(int, int)> { (0, 7), (1, 7) }),
-                new Cage(11, new List<(int, int)> { (0, 8), (1, 8)}),
-                new Cage(21, new List<(int, int)> {(1, 4), (2,4), (2,3)}),
-                new Cage(12, new List<(int, int)> {(1, 5), (2,5) }),
-                new Cage(18, new List<(int, int)> {(1, 6), (2,6), (2,7), (2,8), (3,7) }),
-                new Cage(23, new List<(int, int)> {(2,0), (3,0), (4,0), (5,0) }),
-                new Cage(12, new List<(int, int)> {(3,1), (4, 1), (3, 2)}),
-                new Cage(18, new List<(int, int)> {(3, 3), (4, 3), (5, 3), (4,2)}),
-                new Cage(19, new List<(int, int)> {(3, 4), (4, 4), (5, 4)}),
-                new Cage(16, new List<(int, int)> {(3, 5), (4, 5), (5, 5)}),
-                new Cage(14, new List<(int, int)> {(3, 6), (4, 6), (4, 7)}),
-                new Cage(18, new List<(int, int)> {(3, 8), (4, 8), (5, 8), (6,8)}),
-                new Cage(9, new List<(int, int)> {(5, 1), (5, 2)}),
-                new Cage(12, new List<(int, int)> {(5, 6), (5, 7)}),
-                new Cage(44, new List<(int, int)> {(6, 0), (7, 0), (8, 0), (6,1), (7,1), (8,1), (6,2), (7,2)}),
-                new Cage(15, new List<(int, int)> {(6, 3), (7, 3)}),
-                new Cage(16, new List<(int, int)> {(6, 4), (7, 4), (6, 5)}),
-                new Cage(11, new List<(int, int)> {(6, 6), (6, 7)}),
-                new Cage(9, new List<(int, int)> {(7,5), (7, 6), (8, 6)}),
-                new Cage(24, new List<(int, int)> {(7, 7), (7, 8), (8,7), (8,8)}),
-                new Cage(13, new List<(int, int)> {(8, 2), (8, 3), (8, 4), (8,5)}),
-
-
-                // etc..
-            };
-
-            Stopwatch atimer = Stopwatch.StartNew();
-            KillerSudokuSolver solver = new KillerSudokuSolver(board, cages);
-            var solverRunner = new ForwardTracking();
-            solverRunner.RunForwardTracking();
-            /*            if (solver.Solve())
-                        {
-                            Console.WriteLine("Solved Killer Sudoku:");
-                            solver.PrintBoard();
-                        }
-                        else
-                        {
-                            Console.WriteLine("No solution exists.");
-                        }*/
-            atimer.Stop();
-            Console.WriteLine("Elapsed time: " + atimer.ElapsedMilliseconds + " ms");
+                cellToCage[cell] = cage;
+            }
         }
     }
 
-    public class Cage
+    public bool Solve()
     {
-        public int TargetSum { get; }
-        public List<(int, int)> Cells { get; }
-
-        public Cage(int targetSum, List<(int, int)> cells)
-        {
-            TargetSum = targetSum;
-            Cells = cells;
-        }
+        return ForwardCheck();
     }
 
-    public class KillerSudokuSolver
+    private bool ForwardCheck()
     {
-        private int[,] board;
-        private List<Cage> cages;
-        private const int SIZE = 9;
-
-        public KillerSudokuSolver(int[,] board, List<Cage> cages)
-        {
-            this.board = board;
-            this.cages = cages;
-        }
-        
-        public bool Solve()
-        {
-            for (int row = 0; row < SIZE; row++)
-            {
-                for (int col = 0; col < SIZE; col++)
-                {
-                    if (board[row, col] == 0)
-                    {
-                        for (int num = 1; num <= SIZE; num++)
-                        {
-                            if (IsSafe(row, col, num))
-                            {
-                                board[row, col] = num;
-
-                                if (Solve())
-                                {
-                                    return true;
-                                }
-
-                                board[row, col] = 0; // backtrack
-                            }
-                        }
-                        return false;
-                    }
-                }
-            }
-            return true; // Solved
-        }
-
-        private bool IsSafe(int row, int col, int num)
-        {
-            // Check row
-            for (int x = 0; x < SIZE; x++)
-            {
-                if (board[row, x] == num)
-                {
-                    return false;
-                }
-            }
-
-            // Check column
-            for (int x = 0; x < SIZE; x++)
-            {
-                if (board[x, col] == num)
-                {
-                    return false;
-                }
-            }
-
-            // Check 3x3 subgrid
-            int startRow = row - row % 3;
-            int startCol = col - col % 3;
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (board[i + startRow, j + startCol] == num)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            // Check cages
-            foreach (var cage in cages)
-            {
-                if (cage.Cells.Contains((row, col)))
-                {
-                    int currentSum = 0;
-                    int emptyCells = 0;
-
-                    foreach (var cell in cage.Cells)
-                    {
-                        int r = cell.Item1;
-                        int c = cell.Item2;
-                        if (board[r, c] == num && (r != row || c != col))
-                        {
-                            return false;
-                        }
-                        if (board[r, c] == 0)
-                        {
-                            emptyCells++;
-                        }
-                        currentSum += board[r, c];
-                    }
-
-                    if (currentSum + num > cage.TargetSum)
-                    {
-                        return false;
-                    }
-
-                    if (emptyCells == 1 && currentSum + num != cage.TargetSum)
-                    {
-                        return false;
-                    }
-                }
-            }
-
+        (int row, int col)? next = SelectUnassignedCell();
+        if (!next.HasValue)
             return true;
+
+        int r = next.Value.row;
+        int c = next.Value.col;
+
+        foreach (int num in OrderDomainValues(r, c))
+        {
+            board[r, c] = num;
+            if (IsValid(r, c) && ForwardCheck())
+                return true;
+            board[r, c] = 0;
         }
 
-        public void PrintBoard()
-        {
-            for (int r = 0; r < SIZE; r++)
-            {
-                for (int d = 0; d < SIZE; d++)
-                {
-                    Console.Write(board[r, d]);
-                    Console.Write(" ");
-                }
-                Console.WriteLine();
+        return false;
+    }
 
-                if ((r + 1) % 3 == 0)
+    private (int, int)? SelectUnassignedCell()
+    {
+        int minOptions = int.MaxValue;
+        (int, int)? best = null;
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                if (board[i, j] == 0)
                 {
-                    Console.WriteLine();
+                    var options = GetPossibleValues(i, j);
+                    if (options.Count < minOptions)
+                    {
+                        minOptions = options.Count;
+                        best = (i, j);
+                        if (minOptions == 1) return best;
+                    }
                 }
             }
+        }
+
+        return best;
+    }
+
+    private List<int> OrderDomainValues(int row, int col)
+    {
+        var options = GetPossibleValues(row, col);
+        options.Sort((a, b) => CountConstraints(row, col, a).CompareTo(CountConstraints(row, col, b)));
+        return options;
+    }
+
+    private int CountConstraints(int row, int col, int value)
+    {
+        int count = 0;
+        for (int i = 0; i < Size; i++)
+        {
+            if (board[row, i] == 0) count++;
+            if (board[i, col] == 0) count++;
+        }
+        return count;
+    }
+
+    private List<int> GetPossibleValues(int row, int col)
+    {
+        var possible = Enumerable.Range(1, 9).ToList();
+
+        for (int i = 0; i < Size; i++)
+        {
+            possible.Remove(board[row, i]);
+            possible.Remove(board[i, col]);
+        }
+
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int i = boxRow; i < boxRow + 3; i++)
+            for (int j = boxCol; j < boxCol + 3; j++)
+                possible.Remove(board[i, j]);
+
+        if (cellToCage.TryGetValue((row, col), out var cage))
+        {
+            var filledValues = new HashSet<int>();
+            int sum = 0;
+            int empty = 0;
+
+            foreach (var (r, c) in cage.Cells)
+            {
+                int v = board[r, c];
+                if (v != 0)
+                {
+                    filledValues.Add(v);
+                    sum += v;
+                }
+                else
+                {
+                    empty++;
+                }
+            }
+
+            possible = possible.Where(v => !filledValues.Contains(v) && sum + v <= cage.TargetSum).ToList();
+        }
+
+        return possible;
+    }
+
+    private bool IsValid(int row, int col)
+    {
+        int val = board[row, col];
+
+        for (int i = 0; i < Size; i++)
+        {
+            if (i != col && board[row, i] == val) return false;
+            if (i != row && board[i, col] == val) return false;
+        }
+
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int i = boxRow; i < boxRow + 3; i++)
+            for (int j = boxCol; j < boxCol + 3; j++)
+                if ((i != row || j != col) && board[i, j] == val) return false;
+
+        if (cellToCage.TryGetValue((row, col), out var cage))
+        {
+            int sum = 0;
+            var values = new HashSet<int>();
+            foreach (var (r, c) in cage.Cells)
+            {
+                int v = board[r, c];
+                if (v != 0)
+                {
+                    if (!values.Add(v)) return false;
+                    sum += v;
+                }
+            }
+            if (values.Count == cage.Cells.Count && sum != cage.TargetSum) return false;
+            if (sum > cage.TargetSum) return false;
+        }
+
+        return true;
+    }
+
+    public void PrintBoard()
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var cages = new List<Cage>
+        {
+            new Cage(9,  new List<(int, int)> { (0, 0), (1, 0) }),
+            new Cage(28, new List<(int, int)> { (0, 1), (0, 2), (1, 2), (2, 2), (1,3) }),
+            new Cage(7,  new List<(int, int)> { (1, 1), (2, 1) }),
+            new Cage(11, new List<(int, int)> { (0, 3), (0, 4), (0, 5), (0,6) }),
+            new Cage(15, new List<(int, int)> { (0, 7), (1, 7) }),
+            new Cage(11, new List<(int, int)> { (0, 8), (1, 8)}),
+            new Cage(21, new List<(int, int)> {(1, 4), (2,4), (2,3)}),
+            new Cage(12, new List<(int, int)> {(1, 5), (2,5) }),
+            new Cage(18, new List<(int, int)> {(1, 6), (2,6), (2,7), (2,8), (3,7) }),
+            new Cage(23, new List<(int, int)> {(2,0), (3,0), (4,0), (5,0) }),
+            new Cage(12, new List<(int, int)> {(3,1), (4, 1), (3, 2)}),
+            new Cage(18, new List<(int, int)> {(3, 3), (4, 3), (5, 3), (4,2)}),
+            new Cage(19, new List<(int, int)> {(3, 4), (4, 4), (5, 4)}),
+            new Cage(16, new List<(int, int)> {(3, 5), (4, 5), (5, 5)}),
+            new Cage(14, new List<(int, int)> {(3, 6), (4, 6), (4, 7)}),
+            new Cage(18, new List<(int, int)> {(3, 8), (4, 8), (5, 8), (6,8)}),
+            new Cage(9, new List<(int, int)> {(5, 1), (5, 2)}),
+            new Cage(12, new List<(int, int)> {(5, 6), (5, 7)}),
+            new Cage(44, new List<(int, int)> {(6, 0), (7, 0), (8, 0), (6,1), (7,1), (8,1), (6,2), (7,2)}),
+            new Cage(15, new List<(int, int)> {(6, 3), (7, 3)}),
+            new Cage(16, new List<(int, int)> {(6, 4), (7, 4), (6, 5)}),
+            new Cage(11, new List<(int, int)> {(6, 6), (6, 7)}),
+            new Cage(9, new List<(int, int)> {(7,5), (7, 6), (8, 6)}),
+            new Cage(24, new List<(int, int)> {(7, 7), (7, 8), (8,7), (8,8)}),
+            new Cage(13, new List<(int, int)> {(8, 2), (8, 3), (8, 4), (8,5)})
+        };
+
+        var solver = new KillerSudokuSolver(cages);
+        var stopwatch = Stopwatch.StartNew();
+
+        if (solver.Solve())
+        {
+            stopwatch.Stop();
+            Console.WriteLine("Solved Killer Sudoku:");
+            solver.PrintBoard();
+            Console.WriteLine($"Solved in {stopwatch.ElapsedMilliseconds} ms");
+        }
+        else
+        {
+            stopwatch.Stop();
+            Console.WriteLine("No solution found.");
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
